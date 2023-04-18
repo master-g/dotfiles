2d7a418e4ce8a50b5835ee00803c22f311d3700a
mg <master-g@users.noreply.github.com>
2023-04-18 21:58:39 +0800

    feat: add format on save

--- nvim/custom/configs/null-ls.lua
+++ nvim/custom/configs/null-ls.lua
@@ -22,7 +22,21 @@ local sources = {
   b.formatting.rustfmt,
 }
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup {
   debug = true,
   sources = sources,
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format { bufnr = bufnr }
+        end,
+      })
+    end
+  end,
 }
