d509714c0948e8181b3ae39606850fcca06512d1
Master.G <master-g@users.noreply.github.com>
2024-03-28 12:27:50 +0800

    feat: update

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -1,4 +1,4 @@
--- This file  needs to have same structure as nvconfig.lua 
+-- This file  needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
@@ -10,6 +10,10 @@ M.ui = {
 	hl_override = {
 		Comment = { italic = true },
 		["@comment"] = { italic = true },
+	},
+
+	nvdash = {
+		load_on_startup = true,
 	},
 }
 
